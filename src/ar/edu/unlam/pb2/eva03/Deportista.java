bdfaf43a18576afe0f2a2aaef472b0e60ff23feb
GabrielGomezGG <gabrielgomezgg1997@gmail.com>
2020-11-25 10:22:01 -0300

    No pudo solucionar el problema que tenia con y no me pude 'concentrar'...

---
+++ src/ar/edu/unlam/pb2/eva03/Deportista.java
@@ -0,0 +1,17 @@
+package ar.edu.unlam.pb2.eva03;
+
+import ar.edu.unlam.pb2.eva03.interfaces.*;
+
+public class Deportista implements INadador, ICorredor, ICiclista{
+
+	public void setCantidadDeKilometrosEntrenados(Integer km) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Integer getCantidadDeKilometrosEntrenados() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/ar/edu/unlam/pb2/eva03/Nadador.java
@@ -0,0 +1,7 @@
+package ar.edu.unlam.pb2.eva03;
+
+import ar.edu.unlam.pb2.eva03.interfaces.INadador;
+
+public class Nadador implements INadador{
+	
+}
